0c696a9e440ee6a1d8c0a7ec4324e4e2421e60a1
Albert Tseng <atseng3@gmail.com>
2013-12-26 07:17:26 +0000

    read file to buffer and toString

--- web.js
+++ web.js
@@ -1,14 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync("index.html", function(err, data){
-    if (err) throw err;
-    console.log(data);
-  });
+  var buf = fs.readFileSync('index.html');
 
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
